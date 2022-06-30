8062e524fa06452fa07464ee2b1c29067961936b
Antoine BOURREL <94041592+AntoineBourrel@users.noreply.github.com>
2022-06-30 16:26:13 +0200

    Exercice array avec twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,14 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    //création de la route de la page d'accueil
     /**
      * @Route("/", name="home")
      */
 
+    // méthode pour renvoyer le fichier home.html.twig avec la variable 'name'
     public function home(){
         $name = 'Antoine';
         return $this->render('home.html.twig', [
             'name' => $name
+        ]);
+    }
+
+    /**
+     * @Route ("/article", name="article")
+     */
+
+    public function article(){
+        $category = [
+            'title' => 'Politique',
+            'color' => 'red',
+            'published' => true,
+            'description' => "Decrypter l'actualité politique avec le prisme de David Robert"
+        ];
+        return $this->render('article.html.twig', [
+            'category' => $category
         ]);
     }
 }
